d5f7a1165cd3046cc4f876c158f63a060a99348f
ravinder077@gmail.com <ravinder077@gmail.com>
2021-03-08 22:02:42 +0530

    Initial commit

---
+++ src/main/java/com/tuespotsolutions/first/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.tuespotsolutions.first.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+}
+
+@RestController
+class HelloController
+{
+    @RequestMapping("/hello")
+    String hello()
+    {
+        return "Hello Tuespot";
+    }
+}
